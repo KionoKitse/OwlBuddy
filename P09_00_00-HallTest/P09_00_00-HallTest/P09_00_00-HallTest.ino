ae5f6c3ad82b3208a37ce4443ee43585a50b49fd
KionoKitse <57193934+KionoKitse@users.noreply.github.com>
2020-09-22 22:26:54 -0400

    Update P09_00_00-HallTest.ino
    Search mode tests

--- P09_00_00-HallTest/P09_00_00-HallTest/P09_00_00-HallTest.ino
+++ P09_00_00-HallTest/P09_00_00-HallTest/P09_00_00-HallTest.ino
@@ -3,15 +3,37 @@ int b = 0;
 int c = 0;
 int d = 0;
 
-const int ct = 3;
-double Arr[ct];
+
 int id = 0;
+
+double m = 0;
+
+
+
+//Moving average
+//const int ct = 3;
+//double Arr[ct];
 int tot = 0;
 int avg = 0;
-double m = 0;
-int high = 0;
-int low = 0;
+
+//SearchMode
+const byte Ct = 25; //About half a period
+byte ArrByte[Ct];
+byte Val = 0;
 bool UP = true;
+byte high = 0;
+byte low = 255;
+byte fail = 0;
+//byte FailLim = 10;
+byte LastLow = 0;
+byte LastHigh = 0;
+
+//AvgSearch
+byte FailLim = 1;
+const int ct = 10;
+double Arr[ct];
+
+//All
 int Drop = 570;
 void setup() {
   Serial.begin(115200);
@@ -32,38 +54,194 @@ void loop() {
   //Turn on motor
   digitalWrite(10, 1);
   //Stuff to do with motor on
-  MovingAverage();
+  //MovingAverage();
+  //SearchMode();
+  AvgSearch();
 
   digitalWrite(10, 0);
   delay(20);
 }
-
-void MovingAverage()
+void AvgSearch()
 {
-  for (int i = 0; i < 3; i++)
+  for (int a = 0; a < 2; a++)
   {
+    byte PrintVal = 0.5*(LastHigh-LastLow)+LastLow;
+    
     //Fill array
-    for (int j = 0; j < ct - 1; j++)
+    for (int i = 0; i < ct - 1; i++)
     {
-      Arr[j] = Arr[j + 1];
+      Arr[i] = Arr[i + 1];
     }
     Arr[ct - 1] = analogRead(A0) - Drop;
 
     //Calculate the average value
     tot = 0;
-    for (int j = 0; j < ct; j++)
+    for (int i = 0; i < ct; i++)
     {
-      tot = tot + Arr[j];
+      tot = tot + Arr[i];
     }
     avg = tot / ct;
-    Arr[ct - 1] = avg;
+
+    //Search top
+    if (UP)
+    {
+      if(avg>high)
+      {
+        high = avg;
+        fail = 0;
+      }
+      else
+      {
+        fail++;
+        if(fail > FailLim)
+        {
+          PrintVal = high;
+          LastHigh = high;
+          //Top point found no longer going up
+          UP = false;
+          high = 0;
+          fail = 0;
+        }
+      }
+    }
+    //Search bottom
+    else
+    {
+      if(avg<low)
+      {
+        low = avg;
+        fail = 0;
+      }
+      else
+      {
+        fail++;
+        if(fail > FailLim)
+        {
+          PrintVal = low;
+          LastLow = low;
+          
+          //bottom point found no longer going downp
+          UP = true;
+          low = 255;
+          fail = 0;
+        }
+      }
+    }
+    //Zero point
+    /*
+    if(Val<24)
+    {
+      low = Val;
+      PrintVal = low;
+      LastLow = low;
+      UP = true;
+      low = 255;
+      fail = 0; 
+    }
+    */
 
     //Plot the average value
+    Serial.print(Arr[ct - 1]);
+    Serial.print(",");
     Serial.print(avg);
     Serial.print(",");
-    Serial.print(65);
+    Serial.println(PrintVal);
+    
+  }
+}
+void SearchMode()
+{
+  for (int a = 0; a < 4; a++)
+  {
+    byte PrintVal = 0.5*(LastHigh-LastLow)+LastLow;
+    //Get value
+    Val = analogRead(A0) - Drop;
+
+    //Print
+    Serial.print(Val);
     Serial.print(",");
-    Serial.println(15);
+    
+    //Search top
+    if (UP)
+    {
+      if(Val>high)
+      {
+        high = Val;
+        fail = 0;
+      }
+      else
+      {
+        fail++;
+        if(fail > FailLim)
+        {
+          PrintVal = high;
+          LastHigh = high;
+          //Top point found no longer going up
+          UP = false;
+          high = 0;
+          fail = 0;
+        }
+      }
+    }
+    //Search bottom
+    else
+    {
+      if(Val<low)
+      {
+        low = Val;
+        fail = 0;
+      }
+      else
+      {
+        fail++;
+        if(fail > FailLim)
+        {
+          PrintVal = low;
+          LastLow = low;
+          
+          //bottom point found no longer going downp
+          UP = true;
+          low = 255;
+          fail = 0;
+        }
+      }
+    }
+    //Zero point
+    if(Val<24)
+    {
+      low = Val;
+      PrintVal = low;
+      LastLow = low;
+      UP = true;
+      low = 255;
+      fail = 0; 
+    }
+    Serial.println(PrintVal);
+  }
+}
+void MovingAverage()
+{
+  for (int a = 0; a < 2; a++)
+  {
+    //Fill array
+    for (int i = 0; i < ct - 1; i++)
+    {
+      Arr[i] = Arr[i + 1];
+    }
+    Arr[ct - 1] = analogRead(A0) - Drop;
+
+    //Calculate the average value
+    tot = 0;
+    for (int i = 0; i < ct; i++)
+    {
+      tot = tot + Arr[i];
+    }
+    avg = tot / ct;
+
+    //Plot the average value
+    Serial.print(Arr[ct - 1]);
+    Serial.print(",");
+    Serial.println(avg);
   }
 }
 
