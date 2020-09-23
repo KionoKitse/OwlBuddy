7ddf7e7c3e19d3fbf69821ca9998a58f6aad7a20
KionoKitse <57193934+KionoKitse@users.noreply.github.com>
2020-09-22 20:47:18 -0400

    Update P09_00_00-HallTest.ino
    No Filter mode

--- P09_00_00-HallTest/P09_00_00-HallTest/P09_00_00-HallTest.ino
+++ P09_00_00-HallTest/P09_00_00-HallTest/P09_00_00-HallTest.ino
@@ -15,13 +15,13 @@ bool UP = true;
 int Drop = 570;
 void setup() {
   Serial.begin(115200);
-  pinMode(10,OUTPUT);
-  pinMode(11,OUTPUT);
-  digitalWrite(10,0);
-  digitalWrite(11,0);
-  
+  pinMode(10, OUTPUT);
+  pinMode(11, OUTPUT);
+  digitalWrite(10, 0);
+  digitalWrite(11, 0);
+
   //Fill array with zeros
-  for (int i=0; i<ct; i++)
+  for (int i = 0; i < ct; i++)
   {
     Arr[i] = 0;
   }
@@ -30,38 +30,52 @@ void setup() {
 void loop() {
 
   //Turn on motor
-  digitalWrite(10,1);
+  digitalWrite(10, 1);
   //Stuff to do with motor on
-  for (int i = 0; i < 3; i++) 
-  {
-    MovingAverage();
-  }
-  digitalWrite(10,0);
+  MovingAverage();
+
+  digitalWrite(10, 0);
   delay(20);
 }
 
 void MovingAverage()
 {
-  //Fill array
-  for (int j=0; j<ct-1; j++)
+  for (int i = 0; i < 3; i++)
   {
-    Arr[j] = Arr[j+1];
-  }
-  Arr[ct-1] = analogRead(A0)-Drop;
+    //Fill array
+    for (int j = 0; j < ct - 1; j++)
+    {
+      Arr[j] = Arr[j + 1];
+    }
+    Arr[ct - 1] = analogRead(A0) - Drop;
 
-  //Calculate the average value
-  tot = 0;
-  for (int j=0; j<ct; j++)
+    //Calculate the average value
+    tot = 0;
+    for (int j = 0; j < ct; j++)
+    {
+      tot = tot + Arr[j];
+    }
+    avg = tot / ct;
+    Arr[ct - 1] = avg;
+
+    //Plot the average value
+    Serial.print(avg);
+    Serial.print(",");
+    Serial.print(65);
+    Serial.print(",");
+    Serial.println(15);
+  }
+}
+
+void NoFilter()
+{
+  for (int i = 0; i < 3; i++)
   {
-    tot = tot + Arr[j];
+    //Plot the value
+    Serial.print(analogRead(A0) - Drop);
+    Serial.print(",");
+    Serial.print(65);
+    Serial.print(",");
+    Serial.println(15);
   }
-  avg = tot/ct;
-  Arr[ct-1] = avg;
-
-  //Plot the average value
-  Serial.print(avg);
-  Serial.print(",");
-  Serial.print(65);
-  Serial.print(",");
-  Serial.println(15);
 }
